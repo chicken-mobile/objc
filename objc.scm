5059f8c07615afe7568c6081659523e5ef37a608
mercora <mercora@desktop.mercora.lileth.net>
2013-01-29 13:47:38 +0000

    class-list with naive finalizer (just to now throw it away ;P )

--- objc.scm
+++ objc.scm
@@ -15,19 +15,26 @@
   (foreign-lambda* c-string ((objc-class clazz))
     "C_return(class_getName(*clazz));"))
 
-
 (define (class-list)
   (let ((class-count (objc-get-class-list #f 0)))
     (let-location ((classes objc-class (allocate (* objc-class-size class-count))))
       (let ((return-count (objc-get-class-list classes class-count)))
-	(map (lambda (class-idx)
-	       (pointer+ classes (* objc-class-size class-idx)))
-	     (iota return-count))))))
+	(let ((fin-proc (let ((c return-count))
+			  (lambda (p)
+			    (if (= c 1)
+				(free classes))
+			    (set! c (- c 1))))))
+	  (map (lambda (class-idx)
+		 (set-finalizer! (pointer+ classes (* objc-class-size class-idx)) fin-proc))
+	       (iota return-count)))))))
 
 (for-each 
  (lambda (class)
    (print (class-get-name class)))
  (class-list))
 
+(define keep (car (class-list)))
+(pp (class-get-name keep))
+(set! keep #f)
 
 
