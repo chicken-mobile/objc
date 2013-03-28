a5ef06d822a7cc5091f96b83822a47113221bae5
Patrick Brauer (mercora) <mercora@LiLeth.NET>
2013-03-28 17:44:33 +0000

    made macro for typedefs

--- typedefs.scm
+++ typedefs.scm
@@ -5,38 +5,42 @@
 	   (record-instance-type x) (info x)
 	   (pointer->address (objc-record->objc-ptr x))))
 
-(define-record objc-object pointer)
-(define-record-printer (objc-object x out)
-  (print-objc-record out x object-class-name))
-(define-foreign-type objc-object   (c-pointer "struct objc_object")
-  objc-record->objc-ptr make-objc-object)
+(define-syntax define-objc-type
+  (er-macro-transformer
+   (lambda (x r c)
+     (let* ((type-name (cadr x))
+	    (record-print-proc (cddr x))
+	    (record-print-proc (if (>= (length x) 3)
+				   (caddr x)
+				   (symbol-append type-name '-name)))
+	    (real-type-name (if (= (length x) 4)
+				(cadddr x) type-name))
+
+	    (make-record-proc (symbol-append 'make-objc- type-name))
+	    (s-type (symbol-append 'objc- type-name))
+	    (c-type (symbol-append 'objc_ real-type-name)))
+
+       (let ((%define-record         (r 'define-record))
+	     (%define-record-printer (r 'define-record-printer))
+	     (%print-objc-record     (r 'print-objc-record))
+	     (%define-foreign-type   (r 'define-foreign-type))
+	     (%objc-record->objc-ptr (r 'objc-record->objc-ptr)))
+	 `(begin
+	    (,%define-record ,s-type pointer)
+	    (,%define-record-printer (,s-type x out)
+	      (,%print-objc-record out x ,record-print-proc))
+	    (,%define-foreign-type ,s-type (c-pointer (struct ,c-type))
+	      ,%objc-record->objc-ptr ,make-record-proc)))))))
 
 
-(define-record objc-class pointer)
-(define-record-printer (objc-class x out)
-    (print-objc-record out x class-name))
-(define-foreign-type objc-class (c-pointer "struct objc_class")
-  objc-record->objc-ptr make-objc-class)
-
-
-(define-record objc-meta-class pointer)
-(define-record-printer (objc-meta-class x out)
-  (print-objc-record out x object-class-name))
-(define-foreign-type objc-meta-class    (c-pointer "struct objc_class")
-  objc-record->objc-ptr make-objc-meta-class)
-
-
-(define-record objc-method pointer)
-(define-record-printer (objc-method x out)
-  (print-objc-record out x (compose selector-name method-name)))
-(define-foreign-type objc-method   (c-pointer "struct objc_method")
-  objc-record->objc-ptr make-objc-method)
-
-(define-record objc-selector pointer)
-(define-record-printer (objc-selector x out)
-  (print-objc-record out x selector-name))
-(define-foreign-type objc-selector (c-pointer "struct objc_selector")
-  objc-record->objc-ptr make-objc-selector)
-
+(define-objc-type object object-class-name)
+(define-objc-type class)
+(define-objc-type meta-class class-name class)
+(define-objc-type selector)
+(define-objc-type method (compose selector-name method-name))
 
 (define-foreign-type objc-imp c-pointer)
+
+
+
+
